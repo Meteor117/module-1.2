8da60593df47d5c7f54edf10a00e74bccd152a87
Meteor117 <kharitonov_62@mail.ru>
2024-07-31 19:23:44 +0300

    Add files via upload

---
+++ module1.2.py
@@ -0,0 +1,6 @@
+home_work=12
+work_clock=1.5
+course_name='Python'
+print("Курс:",course_name,',всего задач:',home_work,",затрачено часов:",work_clock, ",среднее время выполнения:",(work_clock)/(home_work), "ч.")
+
+
